af668f642d92ca898a7abfc732058be16ee9bb84
nitnop <covepontin@gmail.com>
2023-08-24 15:31:38 -0600

    last commit before branch

---
+++ FinalProjectDriver.java
@@ -0,0 +1,190 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class FinalProjectDriver {
+    
+    static Scanner UserInput = new Scanner(System.in);
+    static int managerPIN = 1234;
+    static Boolean manager = false;
+    static CopyOnWriteArrayList<Movie> movieList = new CopyOnWriteArrayList<Movie>();
+    static ArrayList<Movie> AtoZmovieTitleList = new ArrayList<Movie>();
+    static int userpassword;
+    static String title;
+    static String actor;
+    static String genre;
+    static String year;
+    static String titleSearch;
+    static int numOfindexToRemove;
+
+    public static void main(String[] args){
+        
+        movieList = Populator.startinglistMovie();
+
+        // user needs a value probably bool and varriable call manager
+
+        System.out.println("Login = 0\nContinue without login = 1");
+
+        int userchoice = UserInput.nextInt();
+
+        switch(userchoice){
+            case 0:
+            System.out.println("Enter Password:");
+            userpassword = UserInput.nextInt();
+            if(userpassword == managerPIN){
+                manager = true;
+                System.out.println("Welcome Manager");
+                
+                do {
+                    UserPromps.getUserprompts(manager);
+                    userchoice = UserInput.nextInt();
+
+                    switch(userchoice){
+                        
+                        case 1:
+
+                        UserInput.nextLine();
+                        System.out.println("Enter Movie title...");
+                        title = UserInput.nextLine();
+                        System.out.println("Enter Movie main actor...");
+                        actor = UserInput.nextLine();
+                        System.out.println("Enter Movie genre...");
+                        genre = UserInput.nextLine();
+                        System.out.println("Enter Movie release year...");
+                        year = UserInput.nextLine();
+                        movieList.add(Populator.addMovie(title, actor, genre, year));
+                        break;
+
+                        case 2:
+                        UserInput.nextLine();
+                        Movie.removeMovie(movieList);
+                        break;
+
+                        case 3:
+                        UserInput.nextLine();
+                        AtoZmovieTitleList = Movie.sortTitleAtoZ(movieList);
+                        for(Movie movtitle : AtoZmovieTitleList){
+                            System.out.println(movtitle);
+                        }                            
+                        break;
+
+                        case 4:
+                        UserInput.nextLine();
+                        Movie.SearchTitle(movieList);
+                        break;
+
+                        case 5:
+                        UserInput.nextLine();
+                        Movie.SearchActor(movieList);
+                        break;
+
+                        case 6:
+                        UserInput.nextLine();
+                        Movie.SearchGenre(movieList);
+                        break;
+
+                        case 7:
+                        UserInput.nextLine();
+                        Movie.SearchYear(movieList);
+                        break;
+
+                        case 8:
+                        break;
+
+                        default:
+                        System.out.println("Stop trying to break my shit and enter a number 1 - 8");
+
+                    }
+
+                }while(userchoice != 8);
+
+            }
+            else{
+                System.out.println("Incorect password");
+            }
+
+            case 1:
+            System.out.println("Welcome User");
+            UserPromps.getUserprompts();
+
+            do {
+                UserPromps.getUserprompts(manager);
+                userchoice = UserInput.nextInt();
+
+                switch(userchoice){
+                    
+                    case 1:
+                    UserInput.nextLine();
+                    Movie.SearchTitle(movieList);
+                    break;
+
+                    case 2:
+                    UserInput.nextLine();
+                    Movie.SearchActor(movieList);
+                    break;
+
+                    case 3:
+                    UserInput.nextLine();
+                    Movie.SearchGenre(movieList);
+                    break;
+
+                    case 4:
+                    UserInput.nextLine();
+                    Movie.SearchYear(movieList);
+                    break;
+
+                    case 5:
+                    UserInput.nextLine();
+                        AtoZmovieTitleList = Movie.sortTitleAtoZ(movieList);
+                        for(Movie movtitle : AtoZmovieTitleList){
+                            System.out.println(movtitle);
+                        }                            
+                        break;
+
+                    default:
+                    System.out.println("Stop trying to break my shit and enter a number 1 - 6");
+                }
+                    
+            }while(userchoice != 6);
+
+        }
+        
+
+        
+
+
+
+
+        // movie object needs title, leading actor, release year, genre
+
+        //toDo:user input and validation probably make a method I can reuse
+
+        //toDo switch statement to fire off Driven
+
+        //method to add movie object
+
+        //method to remove movie object
+
+        //method to set movie title
+
+        //method to set movie actor
+
+        //method to set movie genre
+
+        //method to set movie relese year
+
+        // method to search for a movie name and print it and display it A to Z or Z to A
+
+        // method to search for a movie genre and print it and display it A to Z or Z to A
+        
+        // method to search for a movie actor and print it and display it A to Z or Z to A
+
+        //catch statement
+
+        //some exit text
+
+        
+
+    }
+    
+}
---
+++ Movie.java
@@ -0,0 +1,202 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Movie {
+
+    public Movie(){
+        title = "null";
+        actor = "null";
+        genre = "null";
+        year = "null";
+    }
+
+    public Movie(String titleIn, String actorIn, String genreIn, String yearIn){
+        //Movie movie = new Movie();
+        this.title = titleIn;
+        this.actor = actorIn;
+        this.genre = genreIn;
+        this.year = yearIn;
+        
+    }
+    static String titleSearch;
+    static int numOfindexToRemove;
+    protected String title;
+    protected String actor;
+    protected String genre;
+    protected String year;
+
+    
+    static Scanner UserInput = new Scanner(System.in);
+
+    public void setTitle(){
+        System.out.println("Enter movie title:");
+        this.title = UserInput.next();
+    }
+
+    public void setActor(){
+        System.out.println("Enter movie actor:");
+        this.actor = UserInput.next();
+    }    
+
+    public void setGenre(){
+        System.out.println("Enter movie genre:");
+        this.genre = UserInput.next();
+    }
+
+    public void setYear(){
+        this.year = UserInput.next();
+    }
+
+    static void setWorkingList(){
+
+    }
+    public String toString(){
+        return "[" + title + ", " + actor + ", " + genre + ", " + year + "]";
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public String getActor(){
+        return actor;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public String getYear(){
+        return year;
+    }
+    public static ArrayList<Movie> sortTitleAtoZ(CopyOnWriteArrayList<Movie> movieList){
+        ArrayList<Movie> AtoZmovieList = new ArrayList<Movie>(movieList);
+        AtoZmovieList.sort(Comparator.comparing(Movie::getTitle));
+        return AtoZmovieList;
+    }
+    public static ArrayList<Movie> sortTitleZtoA(CopyOnWriteArrayList<Movie> movieList){
+        ArrayList<Movie> ZtoAmovieList = new ArrayList<Movie>(movieList);
+        ZtoAmovieList.sort(Comparator.comparing(Movie::getTitle));
+        Collections.reverse(ZtoAmovieList);
+        return ZtoAmovieList;
+    }
+    public static ArrayList<Movie> sortActorAtoZ(CopyOnWriteArrayList<Movie> movieList){
+        ArrayList<Movie> AtoZActorList = new ArrayList<Movie>(movieList);
+        AtoZActorList.sort(Comparator.comparing(Movie::getActor));
+        return AtoZActorList;
+    }
+    public static ArrayList<Movie> sortActorZtoA(CopyOnWriteArrayList<Movie> movieList){
+        ArrayList<Movie> ZtoAActorList = new ArrayList<Movie>(movieList);
+        ZtoAActorList.sort(Comparator.comparing(Movie::getActor));
+        Collections.reverse(ZtoAActorList);
+        return ZtoAActorList;
+    }
+    public static ArrayList<Movie> sortGenreAtoZ(CopyOnWriteArrayList<Movie> movieList){
+        ArrayList<Movie> AtoZGenreList = new ArrayList<Movie>(movieList);
+        AtoZGenreList.sort(Comparator.comparing(Movie::getGenre));
+        return AtoZGenreList;
+    }
+    public static ArrayList<Movie> sortGenreZtoA(CopyOnWriteArrayList<Movie> movieList){
+        ArrayList<Movie> ZtoAGenreList = new ArrayList<Movie>(movieList);
+        ZtoAGenreList.sort(Comparator.comparing(Movie::getGenre));
+        Collections.reverse(ZtoAGenreList);
+        return ZtoAGenreList;
+    }
+    public static void removeMovie(CopyOnWriteArrayList<Movie> movieList){
+        System.out.println("What movie would you like to remove from the library? Enter title");
+                        
+        for (Movie mov : movieList){
+            System.out.println(mov);
+        }
+                        
+        titleSearch = UserInput.next();
+
+        for (Movie movieTitle : movieList){
+
+            if (movieTitle.getTitle() != null && movieTitle.getTitle().contains(titleSearch)){
+                                                                
+                System.out.println(movieTitle);                              
+                numOfindexToRemove = movieList.indexOf(movieTitle);
+                //System.out.println("Are you sure you want to delete " + movieTitle);
+                //System.out.println("yes = 0\nno = 1");
+                //int answer = UserInput.nextInt();
+                movieList.remove(numOfindexToRemove);
+            }                           
+        }    
+    }
+
+    public static void SearchTitle(CopyOnWriteArrayList<Movie> movieList){
+
+        System.out.println("Enter the title you'd wish to find...");
+        
+        titleSearch = UserInput.next();
+
+        for (Movie movieTitle : movieList){
+
+            if (movieTitle.getTitle() != null && movieTitle.getTitle().contains(titleSearch)){
+                                                                
+                System.out.println(movieTitle);
+            }
+
+            else {
+                System.out.println("No results...");
+            }
+        }
+    }
+
+    public static void SearchActor(CopyOnWriteArrayList<Movie> movieList){
+        
+        System.out.println("Enter the actor you'd wish to find...");
+
+        String ActorSearch = UserInput.next();
+
+        for (Movie movieActor : movieList){
+
+            if (movieActor.getActor() != null && movieActor.getActor().contains(ActorSearch)){
+                                                                
+                System.out.println(movieActor);
+            }
+
+            else {
+                System.out.println("No results...");
+            }
+        }
+    }
+
+    public static void SearchGenre(CopyOnWriteArrayList<Movie> movieList){
+
+        System.out.println("Enter the genre you'd wish to find...");
+
+        String GenreSearch = UserInput.next();
+
+        for (Movie movieGenre : movieList){
+
+            if (movieGenre.getGenre() != null && movieGenre.getGenre().contains(GenreSearch)){
+                                                                
+                System.out.println(movieGenre);
+            }
+
+            else {
+                System.out.println("No results...");
+            }
+        }
+    }
+
+    public static void SearchYear(CopyOnWriteArrayList<Movie> movieList){
+        System.out.println("Enter the year you'd wish to find...");
+
+        String YearSearch = UserInput.next();
+
+        for (Movie movieYear : movieList){
+
+            if (movieYear.getYear() != null && movieYear.getYear().contains(YearSearch)){
+                                                                
+                System.out.println(movieYear);
+            }
+
+            else {
+                System.out.println("No results...");
+            }
+        }
+    }
+}
