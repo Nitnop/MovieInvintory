42286a69444b6c97e787f28a478379bfdcc66fd7
nitnop <covepontin@gmail.com>
2023-10-31 18:01:18 -0600

    almost got load save implimetned

--- LoadUnload.java
+++ LoadUnload.java
@@ -1,7 +1,43 @@
 // replace populator class with read and write to txt file
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 
 
 public class LoadUnload {
+
+    static Scanner stdIn = new Scanner(System.in);
+    Movie testObject;
+
+    public static void loadFile(){
+        System.out.println("Enter file name");
+        try(ObjectInputStream fileIn = new ObjectInputStream(new FileInputStream(stdIn.nextLine())){
+            while (true){
+                testObject = (Movie) fileIn.readObject();
+                testObject.display();
+            }
+        } // end try and close fileIn automatically
+        catch (EOFException e){
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+        }
+    } //end loadFile method
+
     
+    public static void saveFile(ArrayList<Movie> saveMovie){
+        System.out.println("Enter file name: ");
+
+        try(ObjectInputStream fileOut = new ObjectInputStream(new FileInputStream(stdIn.nextLine())){
+            while (true){
+                fileOut.writeObject(saveMovie);
+            }
+        }
+        catch (EOFException e){
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+        }
+    }
+
+
 }
